98223bf819e610b5670a95f01417ac4398b81fbb
vanshika704 <sharmavanshi704@gmail.com>
2024-08-02 01:12:50 +0530

    first commit

---
+++ read.py
@@ -0,0 +1,14 @@
+import cv2 as cv 
+# capture = cv.VideoCapture(0)
+img = cv.imread("Images/pexels-tdcat-70912.jpg")
+cv.imshow('pexels-tdcat-70912.jpg', img)
+# while True:
+#     isTrue, frame = capture.read()
+#     cv.imshow ('Video', frame)
+
+#     if cv.waitKey(20) & 0xFF==ord('d'):
+#         break
+#     capture.release()
+#     cv.destroyAllWindows()
+
+cv.waitKey(0)
