ee640c37aafd6160b569ee8adbc72afac5a44083
vanshika704 <sharmavanshi704@gmail.com>
2024-08-02 20:35:38 +0530

    .

--- read.py
+++ read.py
@@ -1,16 +1,19 @@
-import cv2 as cv  #import kiya cv2 ko as cv
+import cv2 as cv  # importing cv2 as cv
 
 # img = cv.imread("Images/pexels-tdcat-70912.jpg")
 # cv.imshow('pexels-tdcat-70912.jpg', img)
-capture = cv.VideoCapture(0) # capture naam ka variable bnaya , usme cv.Videocapture ki property lgadi or set krdiya usko 0 pe , jisme 0 web cam ka no hota h , toh is case me ye  webcam kholke dega
-while True: # condition lgai while true ki or neeche  value pass krdi true 
-    isTrue, frame = capture.read() # ab kuki condition true hai , toh kya kro bhyi? read kroge na video ko , toh capture varable ko read property lgadi
+capture = cv.VideoCapture(0)  # creating a capture object and opening webcam
 
-    cv.imshow ('Video', frame) # ab capture toh krlia , show bhi toh krna h , toh imshow krke video ko , frame ko show krdiya
+while True:  # continuously capturing video frames
+    isTrue, frame = capture.read()  # reading the video frame by frame
 
-    if cv.waitKey(20) & 0xFF==ord('d'): # specifications hai samajh  meko aai nahi
-        break # break condition lgdi
-    capture.release()
-    cv.destroyAllWindows()
+    if not isTrue:
+        break  # if frame is not captured properly, exit the loop
 
-# cv.waitKey(0)
+    cv.imshow('Video', frame)  # displaying the video frame
+
+    if cv.waitKey(20) & 0xFF == ord('d'):  # if 'd' key is pressed, break the loop
+        break
+
+capture.release()  # releasing the capture object
+cv.destroyAllWindows()  # closing all OpenCV windows
---
+++ rescale.py
@@ -0,0 +1,25 @@
+import cv2 as cv
+
+# img = cv.imread('Images/pexels-tdcat-70912.jpg')
+# cv.imshow('Cat', img )
+
+def rescaleFrame(frame ,  scale=0.75):
+    width = int(frame.shape[1]* scale)
+    height = int(frame.shape[0]* scale)
+    dimensions = (width,height)
+    return cv.resize(frame ,  dimensions , interpolation = cv.INTER_AREA)
+
+
+capture = cv.VideoCapture('videos/4811927-hd_1920_1080_30fps.mp4')
+while True:
+    isTrue , frame = capture.read()
+    resized_frame = rescaleFrame(frame)
+    cv.imshow ('Video', frame)
+    cv.imshow('video_resized', resized_frame)
+    if cv.waitKey(20) & 0xFF==ord('d'): # specifications hai samajh  meko aai nahi
+        break # break condition lgdi
+
+capture.release()
+cv.destroyAllWindows()
+
+
