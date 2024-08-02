a6ee1a4ba7c22ad06fca5a1f74d26704eac7bbf8
vanshika704 <sharmavanshi704@gmail.com>
2024-08-02 20:08:12 +0530

    read.py

--- read.py
+++ read.py
@@ -1,13 +1,15 @@
-import cv2 as cv 
-capture = cv.VideoCapture(0)
+import cv2 as cv  #import kiya cv2 ko as cv
+
 # img = cv.imread("Images/pexels-tdcat-70912.jpg")
 # cv.imshow('pexels-tdcat-70912.jpg', img)
-while True:
-    isTrue, frame = capture.read()
-    cv.imshow ('Video', frame)
+capture = cv.VideoCapture(0) # capture naam ka variable bnaya , usme cv.Videocapture ki property lgadi or set krdiya usko 0 pe , jisme 0 web cam ka no hota h , toh is case me ye  webcam kholke dega
+while True: # condition lgai while true ki or neeche  value pass krdi true 
+    isTrue, frame = capture.read() # ab kuki condition true hai , toh kya kro bhyi? read kroge na video ko , toh capture varable ko read property lgadi
 
-    if cv.waitKey(20) & 0xFF==ord('d'):
-        break
+    cv.imshow ('Video', frame) # ab capture toh krlia , show bhi toh krna h , toh imshow krke video ko , frame ko show krdiya
+
+    if cv.waitKey(20) & 0xFF==ord('d'): # specifications hai samajh  meko aai nahi
+        break # break condition lgdi
     capture.release()
     cv.destroyAllWindows()
 
